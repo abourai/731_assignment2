e19864319118bddc3d1480e1d6e9cdcefbe722b4
weijianinja <linweijian1994@gmail.com>
2017-03-18 17:59:13 -0400

    fix the state representation issue

--- pbmt/create-phrase-fst.py
+++ pbmt/create-phrase-fst.py
@@ -3,39 +3,48 @@ from collections import defaultdict
 
 
 def make_fst(input_phrases, p_file):
-
+    fst = defaultdict(lambda: defaultdict(int))
     p_file = open(p_file, 'wb')
     with open(input_phrases) as p:
 
         phrases = p.readlines()
         stateid = defaultdict(lambda: len(stateid))
+        node_size = 0
         for phrase in phrases:
         #    print phrase.strip().split('\t')
 
             german = phrase.strip().split('\t')[0].split()
             english = phrase.strip().split('\t')[1].split()
             log_prob = phrase.strip().split('\t')[2]
-            lastId = 0
-            for i in xrange(len(german)):
+            current_id  = 0
+            i = 0
+            while i < len(german) and german[i]+'-g' in fst[current_id]:
+                current_id = fst[current_id][german[i]+'-g']
+                i += 1
+            while i < len(german):
                 g = german[i]
-                if g not in stateid.keys():
-                    print '{} {} {} {}'.format(lastId, stateid[g], g, '<eps>')
-                    p_file.write('{} {} {} {}\n'.format(lastId, stateid[g], g, '<eps>'))
-                    lastId = stateid[g]
-                else:
-                    #print '{} {} {} {}'.format(lastId, stateid[g], g, '<eps>')
-                    lastId = stateid[g]
-                    continue
-
-            for j in xrange(len(english)):
+                node_size +=1
+                fst[current_id][g+'-g'] = node_size
+                print '{} {} {} {}'.format(current_id, node_size, g, '<eps>')
+                p_file.write('{} {} {} {}\n'.format(current_id, node_size, g, '<eps>'))
+                current_id = node_size
+                i += 1
+            
+            j = 0
+            while j < len(english) and english[j]+'-e' in fst[current_id]:
+                current_id = fst[current_id][english[j]+'-e']
+                j += 1
+            while j < len(english):
                 e = english[j]
-                print '{} {} {} {}'.format(lastId, stateid[e], '<eps>', e)
-                p_file.write('{} {} {} {}\n'.format(lastId, stateid[e], '<eps>', e))
-                lastId = stateid[e]
-                #print '{} {} {} {}'.format(lastId, stateid[g], g, '<eps>')
+                node_size +=1
+                fst[current_id][e+'-e'] = node_size
+                print '{} {} {} {}'.format(current_id, node_size, '<eps>',e)
+                p_file.write('{} {} {} {}\n'.format(current_id, node_size,'<eps>',e))
+                current_id = node_size
+                j += 1
 
-            print '{} {} {} {} {}'.format(lastId, 0, '<eps>', '<eps>', log_prob)
-            p_file.write('{} {} {} {} {}\n'.format(lastId, 0, '<eps>', '<eps>', log_prob))
+            print '{} {} {} {} {}'.format(current_id, 0, '<eps>', '<eps>', log_prob)
+            p_file.write('{} {} {} {} {}\n'.format(current_id, 0, '<eps>', '<eps>', log_prob))
         p_file.write('{} {} {} {}\n'.format(0, 0, '</s>', '</s>'))
         p_file.write('{} {} {} {}\n'.format(0, 0, '<unk>', '<unk>'))
         p_file.write('0\n')
